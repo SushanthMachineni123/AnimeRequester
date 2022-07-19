c819e4d8ebe697b0d206e45e3062d23dca147927
SushanthMachneni <106748133+SushanthMachineni123@users.noreply.github.com>
2022-07-19 18:45:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ from alternation import typing_action
 
 bot = Client(
     "notesbot",
-    api_id=os.environ['5441410'],
-    api_hash=os.environ['a1a4fe7d23328f419d98a58339fd9980'],
-    bot_token=os.environ['5574259099:AAHwaZrsBAqmATzCjVplU2Ayy053VKFeJMc'],
+    api_id='5441410'
+    api_hash='a1a4fe7d23328f419d98a58339fd9980'
+    bot_token='5574259099:AAHwaZrsBAqmATzCjVplU2Ayy053VKFeJMc'
 
 )
 
-CHAT_ID = os.environ.get('-1001640127486')
-owner = int(os.environ.get('1451257129'))
+CHAT_ID = '-1001640127486'
+owner = int'1451257129'
 
 
 def call_back_in_filter(data):
